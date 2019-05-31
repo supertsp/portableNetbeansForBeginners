127677051bcf26e34d8844f6a2d7cbd43edb24a3
supertsp <tiago.souza.pedroso@gmail.com>
2019-05-31 11:53:55 -0300

    changing Netbeans template files for Java

---
+++ Netbeans11Portable/user/config/Templates/Classes/Main.java
@@ -0,0 +1,36 @@
+<#if package?? && package != "">
+package ${package};
+</#if>
+
+// <editor-fold defaultstate="collapsed" desc="imports...">
+import static javax.swing.JOptionPane.*;
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="documentation...">
+/**
+ * Objective: ...
+ * 
+ * Description: ...
+ * 
+ * @version 1.0.0
+ * @author ${user}, ${date}, ${time}
+ */// </editor-fold>
+public class ${name} {
+    
+    //<editor-fold defaultstate="collapsed" desc="static attributes...">
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="executable method...">
+    public static void main(String[] args) {
+        
+        
+        
+    }//main
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="static and auxiliary methods...">
+    
+    //</editor-fold>
+    
+}//class

861eb201b3ea70ada23ca5799f501b9c6141a610
supertsp <tiago.souza.pedroso@gmail.com>
2020-09-27 00:41:45 -0300

    update to Netbeans 12.1 and create Java for Beginners Template code

---
+++ user/config/Templates/Classes/BasicMain.java
@@ -0,0 +1,44 @@
+<#if package?? && package != "">
+package ${package};
+</#if>
+
+// <editor-fold defaultstate="collapsed" desc="imports...">
+import static java.lang.Double.parseDouble;
+import static java.lang.Float.parseFloat;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import static javax.swing.JOptionPane.showMessageDialog;
+import static javax.swing.JOptionPane.showInputDialog;
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="documentation...">
+/**
+ * Objective: ...
+ * 
+ * Description: ...
+ * 
+ * @version 1.0.0
+ * @author ${user}, ${date}, ${time}
+ * Last update: -
+ */// </editor-fold>
+public class ${name} {
+
+	// <editor-fold defaultstate="collapsed" desc="static fields...">
+
+	// </editor-fold>
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}//main
+
+	// <editor-fold defaultstate="collapsed" desc="static and auxiliary methods...">
+
+	// </editor-fold>
+
+}//class
